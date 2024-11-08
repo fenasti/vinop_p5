230c53c18a327f48544ede7e26b7ad5f95eefd89
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-08 00:40:31 +0000

    solve files mismatch from json and media

--- wines/admin.py
+++ wines/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Wine
 
 # Register your models here.
+
+class WineAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'grapes',
+        'country',
+        'picture',
+    )
+
+admin.site.register(Wine, WineAdmin)
--- wines/models.py
+++ wines/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Wine(models.Model):
+    name = models.CharField(max_length=100)
+    year = models.CharField(max_length=4)
+    grapes = models.CharField(max_length=100)  
+    country = models.CharField(max_length=100)  
+    region = models.CharField(max_length=100)  
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0.00)
+    picture = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.year})"
+
+    class Meta:
+        ordering = ['name']
