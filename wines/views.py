ac1c4ff9bbce75688e96e9b0efc884196b9245e2
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-08 01:22:00 +0000

    define view all wines links

--- wines/views.py
+++ wines/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Wine
 
 # Create your views here.
+
+def all_wines(request):
+    """ A view to show all wines, including sorting and search queries """
+
+    wines = Wine.objects.all()
+
+    context = {
+        'wines': wines,
+    }
+
+    return render(request, 'wines/wines.html', context)
