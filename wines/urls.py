9b81e8cff35f53bcc038eacee37f2fa4e693ddcb
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-08 01:42:46 +0000

    add wine detail view

--- wines/urls.py
+++ wines/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_wines, name='wines')
+    path('', views.all_wines, name='wines'),
+    path('<wine_id>', views.wine_detail, name='wine_detail'),
 ]
--- wines/views.py
+++ wines/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Wine
 
 # Create your views here.
@@ -13,3 +13,14 @@ def all_wines(request):
     }
 
     return render(request, 'wines/wines.html', context)
+
+def wine_detail(request, wine_id):
+    """ A view to show wines details """
+
+    wine = get_object_or_404(Wine, pk=wine_id)
+
+    context = {
+        'wine': wine,
+    }
+
+    return render(request, 'wines/wine_detail.html', context)
