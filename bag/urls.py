5b64094af6d101d7d3f19f9ec0674701a629adff
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-09 20:28:37 +0000

    creat add to bag a delete view

--- bag/urls.py
+++ bag/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.view_bag, name='view_bag'),
     path('add/<wine_id>/', views.add_to_bag, name='add_to_bag'),
+    path('adjust/<wine_id>/', views.adjust_bag, name='adjust_bag'),
+    path('remove/<wine_id>/', views.remove_from_bag, name='remove_from_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -22,3 +22,30 @@ def add_to_bag(request, wine_id):
     request.session['bag'] = bag
     print(request.session['bag'])
     return redirect(redirect_url)
+
+def adjust_bag(request, wine_id):
+    """Adjust the quantity of the specified wine to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[wine_id] = quantity
+    else:
+        bag.pop(wine_id, None)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, wine_id):
+    """Remove the specified wine from the shopping bag"""
+
+    try:
+        bag = request.session.get('bag', {})
+        bag.pop(wine_id, None)
+        
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
