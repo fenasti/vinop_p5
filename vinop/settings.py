e78febd001a85949ee5b467af294ca22dd0507b4
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-11 16:38:09 +0000

    add static root

--- vinop/settings.py
+++ vinop/settings.py
@@ -187,6 +187,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Local folder for collecting static files
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -214,7 +215,6 @@ if 'USE_AWS' in os.environ:
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
-    
 
 # Stripe
 STRIPE_CURRENCY = 'usd'
