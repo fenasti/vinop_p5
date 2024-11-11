777be01764897ee37f37d3bc4b13d687a7a00895
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-11 13:44:25 +0000

    set settings secret key and debug to variables

--- vinop/settings.py
+++ vinop/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ccp1q2$t)k)4&xuky(-v!*xy@9awcae)f*tiy8e@@^f(t%^z0%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'https://git.heroku.com/vinop-p5.git',
