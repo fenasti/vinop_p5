dd1f77bd929a138a2a47d5fb8020d2949d17f51f
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2024-11-12 22:13:10 +0000

    settings for heroku deployment

--- vinop/settings.py
+++ vinop/settings.py
@@ -29,8 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     'vinop-p5-65ef4c64b7a7.herokuapp.com',
