49ee0ab1aac3cf2b7f4a75ad21a4b3f80e9c5c75
Felipe Nast <td9z0vyv@students.codeinstitute.net>
2025-01-04 18:04:55 +0000

    add model method and implement review stars

--- reviews/models.py
+++ reviews/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 from wines.models import Wine
 
 class Review(models.Model):
-
     RATING_CHOICES = (
         (1, "1"),
         (2, "2"),
@@ -18,6 +17,12 @@ class Review(models.Model):
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def render_stars(self):
+        """Generates HTML for stars based on the rating."""
+        full_stars = '<i class="fas fa-star"></i>' * self.rating
+        empty_stars = '<i class="far fa-star"></i>' * (5 - self.rating)
+        return full_stars + empty_stars
 
     def __str__(self):
         return f'Review by {self.user.username} on {self.wine.name}'
